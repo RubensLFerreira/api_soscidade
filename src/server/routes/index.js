d814c47bfe68e8f612728d2119fcd4b335034e1d
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-04-30 16:00:40 -0300

    Criando rotas de prefeitura

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -7,12 +7,22 @@ import usuarioCreate from '../controllers/usuario/Create.js';
 import usuarioUpdateById from '../controllers/usuario/UpdateById.js';
 import usuarioDeleteById from '../controllers/usuario/DeleteById.js';
 
+import prefeituraGetAll from '../controllers/prefeitura/GetAll.js';
+import prefeituraCreate from '../controllers/prefeitura/Create.js';
+import prefeituraUpdateById from '../controllers/prefeitura/UpdateById.js';
+import prefeituraDeleteById from '../controllers/prefeitura/DeleteById.js';
+
 const router = express.Router();
 
 router.get('/usuarios', usuarioGetAll.getAll);
 router.post('/usuarios', usuarioCreate.create);
 router.put('/usuario/:id', usuarioUpdateById.updateById);
 router.delete('/usuario/:id', usuarioDeleteById.deleteById);
+
+router.get('/prefeituras', prefeituraGetAll.getAll);
+router.post('/prefeituras', prefeituraCreate.create);
+router.put('/prefeitura/:id', prefeituraUpdateById.updateById);
+router.delete('/prefeitura/:id', prefeituraDeleteById.deleteById);
 
 router.get('/', (_, res) => {
   return res.status(StatusCodes.ACCEPTED).send('Hello world! Página inicial');
