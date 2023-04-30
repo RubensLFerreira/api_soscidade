9e0e8b1648be6c3f6ff186b0375f44debaf05a6c
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-04-30 13:28:01 -0300

    Finalizado model usuario

--- src/server/model/Usuario.js
+++ src/server/model/Usuario.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../database';
+import sequelize from '../database/index.js';
 
 const Usuario = sequelize.define('usuario', {
   id: {
@@ -47,8 +47,8 @@ const Usuario = sequelize.define('usuario', {
   }
 });
 
-(async () => {
-  await Usuario.sync({ force: true });
-})();
+// (async () => {
+//   await Usuario.sync({ force: true });
+// })();
 
 export default Usuario;
--- src/server/model/index.js
+++
@@ -1,5 +0,0 @@
-import * as Cliente from './Cliente';
-
-export const modelos = {
-  
-}
