9abe81073aedbe23dd2663ce1a08419df19bc149
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-05-07 16:01:28 -0300

    Criando rotas novas para denuncias

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,31 +2,33 @@ import express from 'express';
 
 import { StatusCodes } from 'http-status-codes';
 
-import usuarioGetAll from '../controllers/usuario/GetAll.js';
-import usuarioCreate from '../controllers/usuario/Create.js';
-import usuarioUpdateById from '../controllers/usuario/UpdateById.js';
-import usuarioDeleteById from '../controllers/usuario/DeleteById.js';
+import cidadaoGetAll from '../controllers/cidadao/GetAll.js';
+import cidadaoCreate from '../controllers/cidadao/Create.js';
+import cidadaoUpdateById from '../controllers/cidadao/UpdateById.js';
+import cidadaoDeleteById from '../controllers/cidadao/DeleteById.js';
 
 import prefeituraGetAll from '../controllers/prefeitura/GetAll.js';
 import prefeituraCreate from '../controllers/prefeitura/Create.js';
 import prefeituraUpdateById from '../controllers/prefeitura/UpdateById.js';
 import prefeituraDeleteById from '../controllers/prefeitura/DeleteById.js';
 
-import problemaCreate from '../controllers/problema/Create.js';
+import denunciaCreate from '../controllers/denuncia/Create.js';
+import denunciaGetAll from '../controllers/denuncia/GetAll.js';
 
 const router = express.Router();
 
-router.get('/usuarios', usuarioGetAll.getAll);
-router.post('/usuarios', usuarioCreate.create);
-router.put('/usuario/:id', usuarioUpdateById.updateById);
-router.delete('/usuario/:id', usuarioDeleteById.deleteById);
+router.get('/cidadaos', cidadaoGetAll.getAll);
+router.post('/cidadaos', cidadaoCreate.create);
+router.put('/cidadao/:id', cidadaoUpdateById.updateById);
+router.delete('/cidadao/:id', cidadaoDeleteById.deleteById);
 
 router.get('/prefeituras', prefeituraGetAll.getAll);
 router.post('/prefeituras', prefeituraCreate.create);
 router.put('/prefeitura/:id', prefeituraUpdateById.updateById);
 router.delete('/prefeitura/:id', prefeituraDeleteById.deleteById);
 
-router.post('/problemas', problemaCreate.create);
+router.get('/denuncias', denunciaGetAll.getAll);
+router.post('/denuncias', denunciaCreate.create);
 
 router.get('/', (_, res) => {
   return res.status(StatusCodes.ACCEPTED).send('Hello world! Página inicial');
