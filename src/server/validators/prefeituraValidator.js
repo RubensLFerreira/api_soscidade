a5ccfd4abfe4ac7f6ad648a2396edc52077eb704
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-05-19 17:20:38 -0300

    Modificando modificações

--- src/server/validators/prefeituraValidator.js
+++ src/server/validators/prefeituraValidator.js
@@ -14,9 +14,9 @@ const prefeituraSchema = yup.object().shape({
     .trim('Não deve conter espaços no início ou no fim'),
   site: yup.string().notRequired(),
   prefeito: yup.string().min(3).required('O campo prefeito é obrigatório'),
-  cidade: yup.string().min(3).required('O campo cidade é obrigatório'),
-  bairro: yup.string().min(3).required('O campo bairro é obrigatório'),
-  rua: yup.string().min(3).required('O campo rua é obrigatório'),
+  // cidade: yup.string().min(3).required('O campo cidade é obrigatório'),
+  // bairro: yup.string().min(3).required('O campo bairro é obrigatório'),
+  // rua: yup.string().min(3).required('O campo rua é obrigatório'),
 });
 
 export default prefeituraSchema;
