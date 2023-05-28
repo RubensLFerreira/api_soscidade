cdd80d2d54272e4633fa0bd54e9df9e3026f82f5
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-05-28 16:04:02 -0300

    feat: rotas da aplicação completa

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import authCidadao from '../middlewares/authCidadao.js';
+import authPrefeitura from '../middlewares/authPrefeitura.js';
 
 import { StatusCodes } from 'http-status-codes';
 
-import cidadaoGetAll from '../controllers/cidadao/GetAll.js';
+
 import cidadaoCreate from '../controllers/cidadao/Create.js';
+import cidadaoGetAll from '../controllers/cidadao/GetAll.js';
 import cidadaoUpdateById from '../controllers/cidadao/UpdateById.js';
 import cidadaoDeleteById from '../controllers/cidadao/DeleteById.js';
 
@@ -17,32 +20,27 @@ import problemaGetAll from '../controllers/problema/GetAll.js';
 import problemaUpdateById from '../controllers/problema/UpdateById.js';
 import problemaDeleteById from '../controllers/problema/DeleteById.js';
 
-import localizacaoCreate from '../controllers/localizacao/Create.js';
-import localizacaoGetAll from '../controllers/localizacao/GetAll.js';
-import localizacaoUpdateById from '../controllers/localizacao/UpdateById.js';
-import localizacaoDeleteById from '../controllers/localizacao/DeleteById.js';
+import usuarioLogin from '../controllers/login/usuarioLogin.js';
+
 
 const router = express.Router();
 
-router.get('/cidadaos', cidadaoGetAll.getAll);
-router.post('/cidadaos', cidadaoCreate.create);
+router.get('/cidadaos', authCidadao, cidadaoGetAll.getAll);
+router.post('/cidadao', cidadaoCreate.create);
 router.put('/cidadao/:id', cidadaoUpdateById.updateById);
 router.delete('/cidadao/:id', cidadaoDeleteById.deleteById);
 
-router.get('/prefeituras', prefeituraGetAll.getAll);
-router.post('/prefeituras', prefeituraCreate.create);
+router.get('/prefeituras', authPrefeitura, prefeituraGetAll.getAll);
+router.post('/prefeitura', prefeituraCreate.create);
 router.put('/prefeitura/:id', prefeituraUpdateById.updateById);
 router.delete('/prefeitura/:id', prefeituraDeleteById.deleteById);
 
 router.get('/problemas', problemaGetAll.getAll);
-router.post('/problemas', problemaCreate.create);
+router.post('/problema', problemaCreate.create);
 router.put('/problema/:id', problemaUpdateById.updateById);
 router.delete('/problema/:id', problemaDeleteById.deleteById);
 
-router.get('/localizacao', localizacaoGetAll.getAll);
-router.post('/localizacao', localizacaoCreate.create);
-router.put('/localizacao/:id', localizacaoUpdateById.updateById);
-router.delete('/localizacao/:id', localizacaoDeleteById.deleteById);
+router.post('/auth/login', usuarioLogin.login);
 
 router.get('/', (_, res) => {
   return res.status(StatusCodes.ACCEPTED).send('Hello world! Página inicial');
