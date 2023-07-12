b22c2a7ed2170a0470f7a877e3288b7ded2ef627
RubensLFerreira <rubenslima000@gmail.com>
2023-07-12 19:45:44 -0300

    recfator: corrigindo validações de problema

--- src/server/validators/problemaValidator.js
+++ src/server/validators/problemaValidator.js
@@ -10,38 +10,30 @@ const problemaSchema = yup.object().shape({
     .max(300, 'Quantidade máxima de 300 caracteres')
     .notRequired(),
   status: yup.boolean(),
-  categoria_id: yup.number()
+  categoria: yup.number()
     .integer('Obrigatório informar id da categoria')
     .moreThan(0, 'Valor do id não pode ser 0')
-    .required('Campo categoria_id é obrigatório'),
-  cidadao_id: yup.number()
+    .required('Campo categoria é obrigatório'),
+  cidadao: yup.number()
     .integer('Obrigatório informar id do cidadão')
     .moreThan(0, 'Valor do id não pode ser 0')
-    .required('Campo cidadao_id é obrigatório'),
-  prefeitura_id: yup.number()
+    .required('Campo cidadao é obrigatório'),
+  prefeitura: yup.number()
     .integer('Obrigatório informar id da prefeitura')
     .moreThan(0, 'Valor do id não pode ser 0')
-    .required('Campo prefeitura_id é obrigatório'),
-  localizacao_id: yup.number()
-    .integer('Obrigatório informar id da localização')
-    .moreThan(0, 'Valor do id não pode ser 0')
-    .required('Campo localizacao_id é obrigatório'),
-  // cidade: yup
-  //   .string()
-  //   .min(3, 'Login precisa ter no minímo 3 caracteres')
-  //   .required('Campo cidade é obrigatório'),
-  // bairro: yup
-  //   .string()
-  //   .min(3, 'Login precisa ter no minímo 3 caracteres')
-  //   .required('Campo bairro é obrigatório'),
-  // rua: yup
-  //   .string()
-  //   .min(3, 'Login precisa ter no minímo 3 caracteres')
-  //   .required('Campo rua é obrigatório'),
-  // tipo: yup
-  //   .string()
-  //   .min(3, 'Login precisa ter no minímo 3 caracteres')
-  //   .required('Campo tipo é obrigatório'),
+    .required('Campo prefeitura é obrigatório'),
+  cidade: yup
+    .string()
+    .min(3, 'Login precisa ter no minímo 3 caracteres')
+    .required('Campo cidade é obrigatório'),
+  bairro: yup
+    .string()
+    .min(3, 'Login precisa ter no minímo 3 caracteres')
+    .required('Campo bairro é obrigatório'),
+  rua: yup
+    .string()
+    .min(3, 'Login precisa ter no minímo 3 caracteres')
+    .required('Campo rua é obrigatório'),
 });
 
 export default problemaSchema;
