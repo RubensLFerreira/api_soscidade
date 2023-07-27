99bf8c4a3104cbf3431ef668f13980988391e3f9
RubensLFerreira <rubenslima000@gmail.com>
2023-07-27 18:27:48 -0300

    refactor: corrigindo erro de tipo de imagem

--- src/server/validators/problemaValidator.js
+++ src/server/validators/problemaValidator.js
@@ -1,24 +1,23 @@
 import * as yup from 'yup';
 
 const problemaSchema = yup.object().shape({
-  imagem: yup
-    .string()
-    .url('Campo imagem deve ser passado URL')
-    .required('Campo imagem é obrigatório'),
   observacao: yup
     .string()
     .max(300, 'Quantidade máxima de 300 caracteres')
     .notRequired(),
   status: yup.boolean(),
-  categoria: yup.number()
+  categoria: yup
+    .number()
     .integer('Obrigatório informar id da categoria')
     .moreThan(0, 'Valor do id não pode ser 0')
     .required('Campo categoria é obrigatório'),
-  cidadao: yup.number()
+  cidadao: yup
+    .number()
     .integer('Obrigatório informar id do cidadão')
     .moreThan(0, 'Valor do id não pode ser 0')
     .required('Campo cidadao é obrigatório'),
-  prefeitura: yup.number()
+  prefeitura: yup
+    .number()
     .integer('Obrigatório informar id da prefeitura')
     .moreThan(0, 'Valor do id não pode ser 0')
     .required('Campo prefeitura é obrigatório'),
